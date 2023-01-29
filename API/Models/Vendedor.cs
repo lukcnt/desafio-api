1b0ff7831bd95d7ef530200b6365599f7046b638
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2023-01-29 15:45:51 -0300

    Criar as propriedades da classe

---
+++ API/Models/Vendedor.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class Vendedor
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+    }
+}
