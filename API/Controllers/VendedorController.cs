a2c31026dda0d7c565fc8ad715e93301d7a1e7d2
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2023-01-30 00:40:33 -0300

    Criar o método para cadastrar vendedor dentro do repository

--- API/Controllers/VendedorController.cs
+++ API/Controllers/VendedorController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.DTO;
+using API.Models;
 using API.Repository;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,6 +18,13 @@ namespace API.Controllers
         public VendedorController(VendedorRepository repository)
         {
             _repository = repository;
+        }
+
+        public IActionResult Cadastrar(CadastrarVendedorDTO dto)
+        {
+            var vendedor = new Vendedor(dto);
+            _repository.Cadastrar(vendedor);
+            return Ok(vendedor);
         }
     }
 }
