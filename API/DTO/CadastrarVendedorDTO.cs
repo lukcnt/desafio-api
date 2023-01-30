c20e3259ed6200fb225281b3dd9a141dacfb4962
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2023-01-29 22:17:43 -0300

    Crie uma DTO para cuidar da transferência de dados ao cadastrar um vendedor

---
+++ API/DTO/CadastrarVendedorDTO.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTO
+{
+    public class CadastrarVendedorDTO
+    {
+        public string Nome { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+    }
+}
