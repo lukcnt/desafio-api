b81044a5615f06f0502e1a3771bf02d59fa06ae9
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2023-01-29 21:40:08 -0300

    Corrigir erro de digitação

--- API/Context/VendasContext.cs
+++ API/Context/VendasContext.cs
@@ -14,11 +14,10 @@ namespace API.Context
 
         }
 
-        public DbSet<Cliente> Cliente { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
         public DbSet<ItemPedido> ItensPedido { get; set; }
         public DbSet<Pedido> Pedidos { get; set; }
         public DbSet<Servico> Servicos { get; set; }
-        public DbSet<Cliente> Clientes { get; set; }
         public DbSet<Vendedor> Vendedores { get; set; }
     }
 }
