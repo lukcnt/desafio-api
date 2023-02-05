66cb742f922dee8945a07b7856c0c89c6529b7bf
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2023-02-05 15:48:19 -0300

    Corrigir erro de digitação

--- API/DTO/AtualizarVendedorDTO.cs
+++ API/DTO/AtualizarVendedorDTO.cs
@@ -7,7 +7,7 @@ namespace API.DTO
 {
     public class AtualizarVendedorDTO
     {
-        public string Name { get; set; }
+        public string Nome { get; set; }
         public string Login { get; set; }
         public string Senha { get; set; }
     }
