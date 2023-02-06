34f13c1367fccbcc3dc9fb68d2aaac703a38bb5f
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2023-02-06 00:32:45 -0300

    Criar o método http para cadastrar pedido

--- API/Controllers/PedidoController.cs
+++ API/Controllers/PedidoController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.DTO;
+using API.Models;
 using API.Repository;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,6 +18,14 @@ namespace API.Controllers
         public PedidoController(PedidoRepository repository)
         {
             _repository = repository;
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(CadastrarPedidoDTO dto)
+        {
+            var pedido = new Pedido(dto);
+            _repository.Cadastrar(pedido);
+            return Ok(pedido);
         }
     }
 }
