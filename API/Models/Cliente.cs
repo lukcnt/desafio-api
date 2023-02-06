e696bed615eaa027a1d838ba57134b6bb60fa217
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2023-02-05 22:27:53 -0300

    Criar o construtor para passar informações para o cadastro do cliente

--- API/Models/Cliente.cs
+++ API/Models/Cliente.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.DTO;
 
 namespace API.Models
 {
@@ -11,5 +12,17 @@ namespace API.Models
         public string Nome { get; set; }
         public string Login { get; set; }
         public string Senha { get; set; }
+
+        public Cliente()
+        {
+
+        }
+
+        public Cliente(CadastrarClienteDTO dto)
+        {
+            Nome = dto.Nome;
+            Login = dto.Login;
+            Senha = dto.Senha;
+        }
     }
 }
