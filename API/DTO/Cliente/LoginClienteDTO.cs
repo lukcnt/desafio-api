ac77f2daab4deeb944c327161d77a258c78c77c3
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2023-02-09 23:18:31 -0300

    Criar a dto para realizar o login como cliente

---
+++ API/DTO/Cliente/LoginClienteDTO.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTO.Cliente
+{
+    public class LoginClienteDTO
+    {
+        public string Login { get; set; }
+        public string Senha { get; set; }
+    }
+}
