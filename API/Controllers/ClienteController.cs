052d2ab41b77e80d456a4e71905ba761c608acdc
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2023-02-09 23:27:03 -0300

    Criar o método http para efetuar login com cliente

--- API/Controllers/ClienteController.cs
+++ API/Controllers/ClienteController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.DTO;
+using API.DTO.Cliente;
 using API.Models;
 using API.Repository;
 using Microsoft.AspNetCore.Mvc;
@@ -26,6 +27,21 @@ namespace API.Controllers
             var cliente = new Cliente(dto);
             _repository.Cadastrar(cliente);
             return Ok(cliente);
+        }
+
+        [HttpPost("EfetuarLogin")]
+        public IActionResult EfetuarLogin(LoginClienteDTO dto)
+        {
+            bool autorizarLogin = _repository.EfetuarLogin(dto);
+
+            if (autorizarLogin)
+            {
+                return Ok(new { Mensagem = "Login autorizado" });
+            }
+            else
+            {
+                return NotFound( new { Mensagem = "Login ou senha não encontrados"});
+            }
         }
     }
 }
