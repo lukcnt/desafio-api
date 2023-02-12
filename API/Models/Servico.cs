fc06e85b5352a76f50fc51ebdadfbb5aff426fda
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2023-02-12 10:33:52 -0300

    Criar os construtores para serviços

--- API/Models/Servico.cs
+++ API/Models/Servico.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.DTO;
 
 namespace API.Models
 {
@@ -10,5 +11,16 @@ namespace API.Models
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Descricao { get; set; }
+
+        public Servico()
+        {
+
+        }
+
+        public Servico(CadastrarServicoDTO dto)
+        {
+            Nome = dto.Nome;
+            Descricao = dto.Descricao;
+        }
     }
 }
