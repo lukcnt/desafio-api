25ffd013f31ed7b0157f0d2a1db0d890e748b06c
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2023-02-12 10:26:24 -0300

    Criar uma dto para passar as informções do serviço

---
+++ API/DTO/Servico/CadastrarServicoDTO.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTO
+{
+    public class CadastrarServicoDTO
+    {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+    }
+}
