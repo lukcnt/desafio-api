f0f404b21e07a91c6454efeae73047651ffefa9d
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2023-02-12 22:36:09 -0300

    Criar os construtores da classe

--- API/Models/ItemPedido.cs
+++ API/Models/ItemPedido.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.DTO;
 
 namespace API.Models
 {
@@ -14,5 +15,18 @@ namespace API.Models
         public Servico Servico { get; set; }
         public int Quantidade { get; set; }
         public decimal Valor { get; set; }
+
+        public ItemPedido()
+        {
+
+        }
+
+        public ItemPedido(CadastrarItemPedidoDTO dto)
+        {
+            PedidoId = dto.PedidoId;
+            ServicoId = dto.ServicoId;
+            Quantidade = dto.Quantidade;
+            Valor = dto.Valor;
+        }
     }
 }
