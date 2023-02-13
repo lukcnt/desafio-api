76ab23405cfec16beff8121a6a922d668357ac6c
Lucas Cantoni <76961476+lukcnt@users.noreply.github.com>
2023-02-12 22:35:36 -0300

    Criar dto para passar informações do cadastro de itens no pedido

---
+++ API/DTO/ItemPedido/CadastrarItemPedidoDTO.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTO
+{
+    public class CadastrarItemPedidoDTO
+    {
+        public int PedidoId { get; set; }
+        public int ServicoId { get; set; }
+        public int Quantidade { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
